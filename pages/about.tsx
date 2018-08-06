fa5591b8a452dff2aea9628081bfee99b76e8d08
akadop <aalahmady@gmail.com>
2018-08-06 07:02:54 -0500

    Add generic component example

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,1 +1,23 @@
-export default () => <div>React-Apollo + TS Example</div>
+import * as React from 'react'
+
+import Goodbye from '../components/Goodbye'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: calc(100% - 2em);
+`
+
+export default () => (
+  <Container>
+    <Goodbye
+      name="random"
+      onClick={() => {
+        console.log('hello')
+      }}
+    />
+  </Container>
+)
